6bf35af8928b595f93ff71ff7d7f91b84c8eb542
JAYapeter <jonathanarta.yapeter@yahoo.com>
2024-10-19 15:00:13 -0400

    change local port flask runs from

--- app.py
+++ app.py
@@ -404,4 +404,4 @@ def session_participants(session_id):
 
 if __name__ == "__main__":
     # Allow access from any IP address
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
